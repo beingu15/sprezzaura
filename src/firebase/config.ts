4a8646b51401ec4b1f8d0badd4bb6245c93ab19b
biju baby <bijubabyofficial@gmail.com>
2025-12-15 05:03:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8359488440-da47e",
+  "appId": "1:971883254440:web:8c134ff46c313e0a99689d",
+  "apiKey": "AIzaSyBJy6mM6wH6XwyyHznMw4Gh5brIogC-q_I",
+  "authDomain": "studio-8359488440-da47e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "971883254440"
+};
