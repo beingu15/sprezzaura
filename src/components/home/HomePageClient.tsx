28670c28fbfe46848d493d651745a2bb470af9a2
biju baby <bijubabyofficial@gmail.com>
2026-02-28 03:18:55 +0000

    Also place the number ticker in a rounder card container

--- src/components/home/HomePageClient.tsx
+++ src/components/home/HomePageClient.tsx
@@ -237,12 +237,14 @@ export default function HomePageClient({ featuredBlogPosts }: HomePageClientProp
       <VideoCarousel />
 
       {/* Number Ticker Section */}
-      <section className="py-12 bg-secondary/95">
+      <section className="py-12 md:py-24 bg-background/95">
         <div className="container mx-auto px-4 md:px-6">
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
-            <NumberTicker value={500} suffix="+" title="Projects Delivered" />
-            <NumberTicker value={10} suffix="+" title="Years of Experience" />
-            <NumberTicker value={50} suffix="+" title="Professional Staff" />
+          <div className="bg-card rounded-[2.5rem] p-12 md:p-16 shadow-xl border border-border/50">
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-12 md:gap-8">
+              <NumberTicker value={500} suffix="+" title="Projects Delivered" />
+              <NumberTicker value={10} suffix="+" title="Years of Experience" />
+              <NumberTicker value={50} suffix="+" title="Professional Staff" />
+            </div>
           </div>
         </div>
       </section>
